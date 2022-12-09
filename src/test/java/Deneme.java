8e068a438481c2bf47e0032f1f7e0cb6dbc6564d
emelgenc <galatasry190538@gmail.com>
2022-12-09 19:22:42 +0300

    Initial commit

---
+++ src/test/java/Deneme.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Deneme {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed","postgres","1234");
+        Statement st = con.createStatement();
+        System.out.println("merhaba");
+
+    }
+}
